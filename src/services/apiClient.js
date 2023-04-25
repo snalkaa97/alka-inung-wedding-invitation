9f1bfb56ea6ff208d038934f3ab5d7a2f35516e1
syaifudin <sfnalkatiri@gmail.com>
2023-04-25 19:05:59 +0700

    BACKEND

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-	baseURL: "https://be-wedding.herokuapp.com",
+	baseURL: "https://be-invitation.onrender.com",
 });
 
 export default apiClient;
